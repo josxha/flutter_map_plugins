5e9546eed8bba02251f4283ce8d8c1218775fe7a
josxha <34318751+josxha@users.noreply.github.com>
2023-06-09 08:40:44 +0200

    create example project

---
+++ example/android/app/src/main/kotlin/com/github/josxha/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.josxha.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- example/flutter_map_cache_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_map_cache/flutter_map_cache.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
