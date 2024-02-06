c685c448f5910ad4da1bddf72c7a9aaa27f03aeb
Joscha <34318751+josxha@users.noreply.github.com>
2024-02-06 22:59:06 +0100

    update SelectionPage on example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map_plugins_example/flutter_map_cache/page.dart';
 import 'package:flutter_map_plugins_example/flutter_map_pmtiles/page.dart';
 import 'package:flutter_map_plugins_example/vector_map_tiles_pmtiles/page.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
@@ -17,7 +22,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
-      home: const Material(child: SelectionPage()),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => const SelectionPage(),
+        'flutter_map_cache': (context) => const FlutterMapCachePage(),
+        'flutter_map_pmtiles': (context) => const FlutterMapPmTilesPage(),
+        'vector_map_tiles_pmtiles': (context) => VectorMapTilesPmTilesPage(),
+      },
     );
   }
 }
@@ -27,67 +38,89 @@ class SelectionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final children = [
+    const children = [
       SelectionItemWidget(
         title: 'flutter_map_cache',
         desc: 'A slim yet powerful caching plugin for flutter_map '
             'tile layers.',
-        pageBuilder: (context) => const FlutterMapCachePage(),
+        routeName: 'flutter_map_cache',
       ),
       SelectionItemWidget(
         title: 'flutter_map_pmtiles',
         desc: 'PMTiles for flutter_map',
-        pageBuilder: (context) => const FlutterMapPmTilesPage(),
+        routeName: 'flutter_map_pmtiles',
       ),
       SelectionItemWidget(
         title: 'vector_map_tiles_pmtiles',
         desc: 'PMTiles for vector_map_files / flutter_map',
-        pageBuilder: (context) => VectorMapTilesPmTilesPage(),
+        routeName: 'vector_map_tiles_pmtiles',
       ),
     ];
 
-    final size = MediaQuery.sizeOf(context);
-
-    if (size.width > 900) {
-      return Padding(
-        padding: const EdgeInsets.all(8),
-        child: GridView.count(
-          crossAxisCount: 2,
-          childAspectRatio: 5,
-          children: children,
+    final width = MediaQuery.sizeOf(context).width;
+    final githubButton = width < 350
+        ? IconButton(onPressed: _openGithub, icon: const Icon(Icons.link))
+        : TextButton(
+            onPressed: _openGithub,
+            child: const Text('Source Code'),
+          );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter_map_plugins'),
+        actions: [githubButton],
+      ),
+      body: ColoredBox(
+        color: Colors.blueGrey.withOpacity(0.3),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: GridView.count(
+            crossAxisCount: max(width ~/ 250, 1),
+            childAspectRatio: 2,
+            children: children,
+          ),
         ),
-      );
-    }
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: ListView(
-        children: children,
       ),
     );
   }
+
+  void _openGithub() => launchUrlString(
+        'https://github.com/josxha/flutter_map_plugins',
+      );
 }
 
 class SelectionItemWidget extends StatelessWidget {
+  static const _titleStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
   final String title;
   final String desc;
-  final WidgetBuilder pageBuilder;
+  final String routeName;
 
   const SelectionItemWidget({
     super.key,
     required this.title,
     required this.desc,
-    required this.pageBuilder,
+    required this.routeName,
   });
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.white,
       child: InkWell(
-        onTap: () =>
-            Navigator.of(context).push(MaterialPageRoute(builder: pageBuilder)),
-        child: ListTile(
-          title: Text(title),
-          subtitle: Text(desc),
+        onTap: () => Navigator.of(context).pushNamed(routeName),
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(title, style: _titleStyle, textAlign: TextAlign.center),
+              const Spacer(),
+              Text(desc, textAlign: TextAlign.center),
+              const Spacer(),
+            ],
+          ),
         ),
       ),
     );
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import connectivity_plus
 import path_provider_foundation
 import sqlite3_flutter_libs
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
