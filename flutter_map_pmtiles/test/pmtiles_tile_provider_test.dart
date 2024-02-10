07a3d424bca75b9c56d7ff595f61575b67e75ef8
Joscha <34318751+josxha@users.noreply.github.com>
2024-02-10 17:56:51 +0100

    add tests

--- flutter_map_pmtiles/test/pmtiles_tile_provider_test.dart
+++ flutter_map_pmtiles/test/pmtiles_tile_provider_test.dart
@@ -9,4 +9,11 @@ Future<void> main() async {
     final provider = PmTilesTileProvider.fromArchive(mockPmTiles);
     expect(provider.archive, equals(mockPmTiles));
   });
+  test('Create tile provider from source', () async {
+    const source =
+        'https://raw.githubusercontent.com/protomaps/PMTiles/main/spec/v3/stamen_toner(raster)CC-BY%2BODbL_z3.pmtiles';
+    final provider = await PmTilesTileProvider.fromSource(source);
+    expect(provider.archive.centerPosition.latitude, closeTo(0, 0.01));
+    expect(provider.archive.centerPosition.longitude, closeTo(0, 0.01));
+  });
 }
--- vector_map_tiles_pmtiles/test/pmtiles_tile_provider_test.dart
+++ vector_map_tiles_pmtiles/test/pmtiles_tile_provider_test.dart
@@ -1,4 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pmtiles/pmtiles.dart';
+import 'package:vector_map_tiles/vector_map_tiles.dart';
 import 'package:vector_map_tiles_pmtiles/src/vector_tile_provider.dart';
 
 import 'integration_test.mocks.dart';
@@ -8,5 +12,45 @@ Future<void> main() async {
     final mockPmTiles = MockPmTilesArchive();
     final provider = PmTilesVectorTileProvider.fromArchive(mockPmTiles);
     expect(provider.archive, equals(mockPmTiles));
+  });
+  test('Create tile provider from source', () async {
+    const source =
+        'https://raw.githubusercontent.com/protomaps/PMTiles/main/spec/v3/protomaps(vector)ODbL_firenze.pmtiles';
+    final provider = await PmTilesVectorTileProvider.fromSource(source);
+    expect(
+      provider.archive.centerPosition.latitude,
+      closeTo(43.779779, 0.001),
+    );
+    expect(
+      provider.archive.centerPosition.longitude,
+      closeTo(11.2414827, 0.001),
+    );
+    expect(provider.silenceTileNotFound, isFalse);
+    expect(provider.maximumZoom, equals(14));
+    expect(provider.minimumZoom, equals(0));
+    expect(
+      await provider.provide(TileIdentity(0, 0, 0)),
+      isA<Uint8List>(),
+    );
+    await expectLater(
+      provider.provide(TileIdentity(10, 1, 1)),
+      throwsA(isA<TileNotFoundException>()),
+    );
+  });
+  test('Create silenced tile provider', () async {
+    const source =
+        'https://raw.githubusercontent.com/protomaps/PMTiles/main/spec/v3/protomaps(vector)ODbL_firenze.pmtiles';
+    final provider = await PmTilesVectorTileProvider.fromSource(
+      source,
+      silenceTileNotFound: true,
+    );
+    expect(
+      await provider.provide(TileIdentity(0, 0, 0)),
+      isA<Uint8List>(),
+    );
+    await expectLater(
+      await provider.provide(TileIdentity(10, 1, 1)),
+      equals(Uint8List(0)),
+    );
   });
 }
