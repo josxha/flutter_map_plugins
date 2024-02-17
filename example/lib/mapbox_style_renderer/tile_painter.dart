02b124bcf26dafe83a0202ca17c5cb560952a865
Joscha <34318751+josxha@users.noreply.github.com>
2024-02-17 23:34:20 +0100

    parse feature command and parameters

--- example/lib/mapbox_style_renderer/tile_painter.dart
+++ example/lib/mapbox_style_renderer/tile_painter.dart
@@ -45,16 +45,48 @@ class TilePainter extends CustomPainter {
             canvas.drawPath(path, paint);
           case VectorTileGeomType.LINESTRING:
             if (feature.geometryList == null) break;
+
             final paint = Paint()
               ..color = Colors.blue
               ..strokeWidth = 1;
             final path = Path();
-            for (var i = 1; i < feature.geometryList!.length; i += 2) {
-              final x = feature.geometryList![i - 1] / 100.0;
-              final y = feature.geometryList![i] / 100.0;
-              path.lineTo(x, y);
+
+            final geom = feature.geometryList!;
+            var i = 0;
+            while (i < geom.length) {
+              // parse command integer
+              final commandInteger = geom[i++];
+              final command = GeometryCommand.parse(commandInteger & 0x7);
+              if (command == null) {
+                debugPrint(
+                  'Could not parse the command id of the command '
+                  'integer $commandInteger.',
+                );
+                return;
+              }
+              final count = commandInteger >> 3;
+
+              // decode zigzag encoded parameter integers
+              switch (command) {
+                case GeometryCommand.moveTo:
+                  for (var p = 0; p < count; p++) {
+                    path.moveTo(
+                      parseParamInt(geom[i++]),
+                      parseParamInt(geom[i++]),
+                    );
+                  }
+                case GeometryCommand.lineTo:
+                  for (var p = 0; p < count; p++) {
+                    path.relativeLineTo(
+                      parseParamInt(geom[i++]),
+                      parseParamInt(geom[i++]),
+                    );
+                  }
+                case GeometryCommand.closePath:
+                  path.close();
+              }
+              canvas.drawPath(path, paint);
             }
-            canvas.drawPath(path, paint);
           case null:
             debugPrint('feature cannot be rendered: $feature');
         }
@@ -65,3 +97,23 @@ class TilePainter extends CustomPainter {
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
+
+enum GeometryCommand {
+  moveTo(2),
+  lineTo(2),
+  closePath(0);
+
+  final int paramCount;
+
+  const GeometryCommand(this.paramCount);
+
+  static GeometryCommand? parse(int id) => switch (id) {
+        1 => GeometryCommand.moveTo,
+        2 => GeometryCommand.lineTo,
+        7 => GeometryCommand.closePath,
+        _ => null,
+      };
+}
+
+double parseParamInt(int paramInt) =>
+    ((paramInt >> 1) ^ (-(paramInt & 1))).toDouble();
