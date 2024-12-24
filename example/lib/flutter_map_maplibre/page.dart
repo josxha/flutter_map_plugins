5a7906d7f6be008a2e22406f4029aa2f2e6e4589
Joscha <34318751+josxha@users.noreply.github.com>
2024-12-25 00:03:00 +0100

    fix deprecations

--- example/lib/flutter_map_maplibre/page.dart
+++ example/lib/flutter_map_maplibre/page.dart
@@ -86,7 +86,7 @@ class _MapLibreFlutterMapPageState extends State<MapLibreFlutterMapPage> {
                   LatLng(5, -10),
                   LatLng(10, -15),
                 ],
-                color: Colors.pink.withOpacity(0.8),
+                color: Colors.pink.withValues(alpha: 0.8),
               ),
             ],
           ),
