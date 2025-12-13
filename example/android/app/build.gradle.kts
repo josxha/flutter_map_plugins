17109cc68f0a84e1b0981b9583f977d0b15fa0cb
Joscha <34318751+josxha@users.noreply.github.com>
2025-12-13 11:17:57 +0100

    update example project

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.flutter_map_plugins_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // flutter.ndkVersion
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
